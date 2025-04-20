27e1f0e82723bf850eff1a71ebb1cc789a4074b7
Jonathan1324 <mohrjonathan09@gmail.com>
2025-04-20 19:13:27 +0200

    refactor: remove unused RuleSave and RuleSetSave structures from code

--- src/data/aidata/ruleset.hpp
+++ src/data/aidata/ruleset.hpp
@@ -33,37 +33,3 @@ struct RuleSet {
 };
 
 #endif
-
-/*
-typedef struct {
-    uint16_t input_id;
-    uint8_t op;
-    uint16_t compare_input_id;
-    uint64_t compare_value;
-
-    uint8_t action_type;
-//  0: then rule,   else rule,      compare to value
-//  1: then action, else rule,      compare to value
-//  2: then rule,   else action,    compare to value
-//  3: then action, else action,    compare to value
-//  4: then rule,   else rule,      compare to input
-//  5: then action, else rule,      compare to input
-//  6: then rule,   else action,    compare to input
-//  7: then action, else action,    compare to input
-
-    uint64_t then_rule_id;
-    uint64_t else_rule_id;
-
-    uint16_t then_output_id;
-    uint64_t then_output_value;
-
-    uint16_t else_output_id;
-    uint64_t else_output_value;
-} RuleSave;
-
-typedef struct {
-    uint64_t rule_count;
-    uint64_t root_rule_id;
-    RuleSave* rules;
-} RuleSetSave;
-*/
--- src/main.cpp
+++ src/main.cpp
@@ -38,42 +38,5 @@ int main() {
 
     saveData(&data, "data.dat");
 
-    /*
-    DataSave data;
-
-    initialize_dataSave(&data, 1, 1, 1);
-
-    
-    strcpy(data.metadata->name, "Test Metadata");
-    data.metadata->type = RULE_BASED;
-
-    strcpy(data.metadata->inputs[0].name, "Input 1");
-    data.metadata->inputs[0].id = 0;
-
-    strcpy(data.metadata->inputs[1].name, "Input 2");
-    data.metadata->inputs[1].id = 1;
-
-    strcpy(data.metadata->outputs[0].name, "Output 1");
-    data.metadata->outputs[0].id = 0;
-
-
-    data.aidata->ruleset->rules[0].input_id = 0;
-    data.aidata->ruleset->rules[0].op = 1;
-    data.aidata->ruleset->rules[0].compare_input_id = 1;
-    data.aidata->ruleset->rules[0].compare_value = 100;
-    data.aidata->ruleset->rules[0].action_type = 0;
-    data.aidata->ruleset->rules[0].then_rule_id = 0;
-    data.aidata->ruleset->rules[0].else_rule_id = 0;
-    data.aidata->ruleset->rules[0].then_output_id = 0;
-    data.aidata->ruleset->rules[0].then_output_value = 1;
-    data.aidata->ruleset->rules[0].else_output_id = 0;
-    data.aidata->ruleset->rules[0].else_output_value = 2;
-
-
-    writeFile("data.dat", &data);
-
-    destroy_dataSave(&data);
-    */
-
     return 0;
 }
